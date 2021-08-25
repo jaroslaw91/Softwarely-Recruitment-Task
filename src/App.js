4ca1fa9c7552943271bd2f58e4e917b963ebc66d
jaroslaw91 <jaroslawkucharski91@gmail.com>
2021-08-25 19:05:31 +0200

    Added app index

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'assets/styles/theme.scss'
+
+const App = () => <div>Softwarely Recruitment Task</div>
+
+ReactDOM.render(<App />, document.getElementById('root'))
